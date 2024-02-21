6c94c7c600394e09e1f3ea2d4c392aab6fc481b7
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-02-21 10:36:40 -0500

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,14 @@
+provider "azurerm" {
+  features {}
+}
+
+
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
